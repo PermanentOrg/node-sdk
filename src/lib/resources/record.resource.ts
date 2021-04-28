349d69a05d932257621b87120e4a03e144d99822
Cecilia Krum <cecilia@permanent.org>
2021-04-28 13:57:13 -0500

    Consolidate registerRecord and addStorage
    Remove duplicate functions and make one call to the backend.  I was
    able to verify that this does work - that is, the record is
    registered and the right amount of account space is added.  Next is to
    consolidate these calls on the backend.

--- src/lib/resources/record.resource.ts
+++ src/lib/resources/record.resource.ts
@@ -57,19 +57,6 @@ export class RecordResource extends BaseResource {
     return this.getVoFromResponse<RecordResponse>(response, 'RecordVO');
   }
 
-  public async addStorage(record: RecordVO) {
-    const accountResponse = await this.api.account.getSessionAccount();
-    const account = accountResponse.Results[0].data[0].AccountVO;
-    const billingResponse = await this.api.billing.addStorage(
-      account.accountId,
-      record.size
-    );
-    return this.getVoFromResponse<BillingResponse>(
-      billingResponse,
-      'LedgerNonfinancialVO'
-    );
-  }
-
   public async getPresignedUrl(fileType: string, record: RecordVO) {
     const response = await this.api.record.getPresignedUrl(fileType, record);
     if (!response.isSuccessful) {
@@ -81,8 +68,36 @@ export class RecordResource extends BaseResource {
     return this.getVoFromResponse<SimpleVOResponse>(response, 'SimpleVO');
   }
 
-  public async registerRecord(record: RecordVO, s3url: string) {
+  public async registerRecordAndAddStorage(record: RecordVO, s3url: string) {
     const response = await this.api.record.registerRecord(record, s3url);
-    return this.getVoFromResponse<RecordResponse>(response, 'RecordVO');
+    if (!response.isSuccessful) {
+      throw new PermSdkError(
+        'record could not be registered',
+        response.Results[0].message
+      );
+    }
+    const returnedRecord = this.getVoFromResponse<RecordResponse>(response, 'RecordVO');
+    const accountResponse = await this.api.account.getSessionAccount();
+    if (!accountResponse.isSuccessful) {
+      throw new PermSdkError(
+        'could not get the session account',
+        response.Results[0].message
+      );
+    }
+    const account = accountResponse.Results[0].data[0].AccountVO;
+    // We have to use the size reported by the client, because at this
+    // point in processing the server-side record does not yet have a size
+    const billingResponse = await this.api.billing.addStorage(
+      account.accountId,
+      record.size
+    );
+    if (!billingResponse.isSuccessful) {
+      throw new PermSdkError(
+        'failed to add storage',
+        response.Results[0].message
+      );
+    }
+    return returnedRecord;
+
   }
 }
